b46ee73719fd2c7d45783b0f090175d7e329cabe
tamires <tamiresarcelino@gmail.com>
2023-11-04 18:07:21 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question: "Qual o significado do status code 201 em resposta http?",
+    answers: [
+      { option: "CREATED", correct: true },
+      { option: "BAD REQUEST", correct: false },
+      { option: "OK", correct: false },
+    ],
+  },
+  {
+    question: "Qual o significado da sigla CRUD em html?",
+    answers: [
+      { option: "CREATED READ UPDATE DELETE", correct: true },
+      { option: "CAMADA REPOSITORY UPDATE DOMIN", correct: false },
+      { option: "CODE RIGHT UP DELETE", correct: false },
+    ],
+  },
+  {
+    question: "Qual a responsabilidade da camada repository em uma aplicação BACK-AND?",
+    answers: [
+      { option: "Gerencia Interação com Usuario", correct: false },
+      { option: "Garenciar Aplicação em outro serviços", correct: false },
+      { option: "Gerencia Interação com Banco de Dados", correct: true },
+    ],
+  },
+  {
+    question: "O significa o status code 404 em uma resposta http?",
+    answers: [
+      { option: "CREATED", correct: false },
+      { option: "BAD REQUEST", correct: false },
+      { option: "NOT FOUND", correct: true },
+    ],
+  },
+  {
+    question: "O que significa HTTP?",
+    answers: [
+      {option: "HyperText Transfer Protocol", correct: true},
+      {option: "HyperText Transfer Package", correct: false},
+      {option: "HyperText Transfer Protom", correct: false},
+    ]
+  }
+];
